fbe71e50c2082bb386bea59b4a6d65a2ac041d99
杨明亮 <1443250341@qq.com>
2026-03-02 16:37:17 +0800

    HashMap v1.2.1

--- src/main/java/Basic/AboutHashMap.java
+++ src/main/java/Basic/AboutHashMap.java
@@ -19,6 +19,11 @@ public class AboutHashMap {
         Map<String, String> map = new HashMap<>();
         map.put("key1", "value1");
         map.put("key2", "value2");
+        map.put("key1", "value2");
         System.out.println(map.get("key1").equals("value2") ? 0 : 1);
+        System.out.println(map);
+        log.info("HashMap测试结束");
+
+        log.info("1 << 30 is : {}", 1 << 30);
     }
 }
